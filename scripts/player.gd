83f771470b4490791f59f85a06fff5f1c241d55c
Owais Ahmad <you@example.com>
2025-12-09 18:51:08 +0530

    Add bullet snipe assets and imports

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -850.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
+
 func _physics_process(delta: float) -> void:
 	#add animations
 	if velocity.x > 1 or velocity.x < -1:
