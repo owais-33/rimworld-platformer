dfb64ed4f11c01b9b3750352ef1654fa24b9c426
Owais Ahmad <you@example.com>
2025-12-31 08:43:21 +0530

    added new scripts for movement and shooting

--- scripts/buttet.gd
+++ scripts/buttet.gd
@@ -1,19 +1,20 @@
 extends Area2D
-
 var speed = 400
 var direction = Vector2.RIGHT
 var init_position = Vector2.ZERO 
+var shooter = null
 
 func _ready():
-	# Connect to detect hits
 	body_entered.connect(_on_body_entered)
 	global_position = init_position
+	global_position += direction * 20
 
 func _process(delta):
 	position += direction * speed * delta
 
 func _on_body_entered(body):
-	# Handle what happens when projectile hits something
+	if body == shooter:
+		return
 	if body.has_method("take_damage"):
 		body.take_damage(10)
-	queue_free()  # Destroy the projectile
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,45 +2,67 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0   
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
 var projectile_scene = preload("res://scripts/buttet.tscn")
 
+var shoot_timer = 0.0
+var is_shooting = false
+var last_direction = 1  
+
 func _physics_process(delta: float) -> void:
-	#add animations
-	if velocity.x > 1 or velocity.x < -1:
-		animated_sprite_2d.animation = "run"
-	else:     
-		animated_sprite_2d.animation = "idle"
-	# Add the gravity.
+	
+	if is_shooting:
+		shoot_timer -= delta
+		if shoot_timer <= 0:
+			is_shooting = false
+	
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	# Handle jump.
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 	var direction := Input.get_axis("left", "right")
+	
+	
+	if direction != 0:
+		last_direction = direction
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	
 	if direction == 1: 
 		animated_sprite_2d.flip_h = false
 	elif direction == -1: 
 		animated_sprite_2d.flip_h = true
+	
+	
+	if not is_shooting:
+		if velocity.x > 1 or velocity.x < -1:
+			animated_sprite_2d.animation = "run"
+		else:     
+			animated_sprite_2d.animation = "idle"
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		shoot_towards_mouse()
 
 func shoot_towards_mouse():
-	animated_sprite_2d.animation = "shoot"
+	
+	animated_sprite_2d.play("shoot")
+	is_shooting = true
+	shoot_timer = 0.5
+	
+	
 	var projectile = projectile_scene.instantiate()
 	projectile.init_position = global_position
+	projectile.shooter = self
 	
-	# Calculate direction to mouse
-	var mouse_pos = get_global_mouse_position()
-	projectile.direction = (mouse_pos - global_position).normalized()
+	
+	projectile.direction = Vector2(last_direction, 0)
 	
 	get_parent().add_child(projectile)
